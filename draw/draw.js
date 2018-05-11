c2a980b1bde2a7d7c27acb21507ed90c900fc67b
WuXiaoTon <974124343@qq.com>
2018-05-11 12:02:34 +0800

    complete

--- draw/draw.js
+++ draw/draw.js
@@ -1,14 +1,25 @@
-   var arr=[]
-   var draw=function lotteryDraw(){
+var serial;
+var max=0;
+var draw=function lotteryDraw(){
         var sum=document.getElementById('sum').value;
         var num=document.getElementById('num');
+        var summ=parseInt(sum);
         if(sum!=null){
-            arr=[];
+            var arr_t=new Array(summ);
+	        arr_t.fill(0);
             var rand;
-            for(var i=0;i<10;i++){
-                arr.push(Math.round(Math.random()*(sum-1)+1));
+            for(var i=0;i<20;i++){
+		        serial=Math.round(Math.random()*(sum-1)+1);
+                arr_t[serial-1]+=1;
             }
-            rand=arr.join(",");
+        for(var j=0;j<summ;j++){
+            if(arr_t[j]>arr_t[max]){
+                max=j;
+                console.log("already");
+            }
+        }
+            rand=max+1;
             num.innerHTML = '<h1 style="color: lightcoral;">'+rand+'</h1>';
+
         }
     }
